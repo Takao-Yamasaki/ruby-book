ad29e74d4daa13aadd190ceeac92f139dc769166
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2025-11-23 17:14:18 +0900

    add:仮実装

--- lib/gate.rb
+++ lib/gate.rb
@@ -1,2 +1,16 @@
 class Gate
+  # 初期化
+  def initialize(name)
+    @name = name
+  end
+  
+  # 入場メソッド
+  def enter(ticket)
+    true
+  end
+
+  # 出場メソッド
+  def exit(ticket)
+    true
+  end
 end
--- test/gate_test.rb
+++ test/gate_test.rb
@@ -1,9 +1,19 @@
+######################
+# 実行方法
+# ruby test/gate_test.rb
+######################
 require 'minitest/autorun'
 require_relative '../lib/gate'
+require_relative '../lib/ticket'
 
 class GateTest < Minitest::Test
   def test_gate
-    # Gateオブジェクトが作成できることを確認
-    assert Gate.new
+    umeda = Gate.new(:umeda)
+    juso = Gate.new(:juso)
+
+    ticket = Ticket.new(160)
+    umeda.enter(ticket)
+    # 期待値: 出場できるので、trueとなる
+    assert juso.exit(ticket)
   end
 end
