0f19ad628e78b44a666398a434c45b44a5c7b8dc
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2025-12-23 10:15:50 +0900

    add:rainbow methodの実装

--- lib/rainbowable.rb
+++ lib/rainbowable.rb
@@ -1,5 +1,14 @@
 module Rainbowable
   def rainbow
-    # 実装はあとで
+    # 文字列表現を取得する
+    str = self.to_s
+    p str
+
+    # ループカウンタ
+    count = 0
+    # 色付けした文字を格納する配列
+    colored_chars = []
+
+    # TODO: ここからやる
   end
 end
